6e647c1bb05c74085b2c24f8bc103e1188149221
Qing_ning <1750359613@qq.com>
2020-10-31 10:16:05 +0800

    Init

---
+++ main.c
@@ -0,0 +1,22 @@
+# include<stdio.h>
+# include<malloc.h>
+
+void turn(char* content){
+
+}
+
+char* input(char* path){
+
+}
+
+int output(char* path, char* content){
+
+}
+
+int main(){
+
+    printf("Hello world!");
+
+}
+
+
